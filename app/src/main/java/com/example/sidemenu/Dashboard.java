8346c4baf842c70d98dcc374c27f06a80f4350be
karthik <skarthi@veemak.com>
2021-09-11 12:37:57 +0530

    First Due Date Idea

--- app/src/main/java/com/example/sidemenu/Dashboard.java
+++ app/src/main/java/com/example/sidemenu/Dashboard.java
@@ -6,11 +6,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 
+import java.util.Calendar;
+
 public class Dashboard extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
 
     // Initialize variable
     DrawerLayout drawerLayout;
@@ -21,6 +30,10 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        initDatePicker();
+        dateButton = findViewById(R.id.due_date_picker_button);
+        dateButton.setText(getTodaysDate());
+
 
         // assign Variable
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -42,10 +55,82 @@ public class Dashboard extends AppCompatActivity {
 
     }
 
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return (makeDateString(day, month, year));
+    }
+
+    private void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+                    String date = makeDateString(day, month, year);
+                    dateButton.setText(date);
+
+            }
+        };
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+//        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+
+    private String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year + " ";
+
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        // default should never happen
+        return "JAN";
+
+    }
+    ;
+
     @Override
     protected void onPause() {
         super.onPause();
         // close drawer
         MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void openDatePicker(View view) {
+        datePickerDialog.show();
     }
 }
